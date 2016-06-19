f82d40f65caa78ae09ea845b12db667f7cb34b20
cclauss <cclauss@bluewin.ch>
2016-06-19 10:29:25 +0200

    Update get_one_get_many.py

--- get_one_get_many.py
+++ get_one_get_many.py
@@ -1,11 +1,15 @@
 import asyncio
+try:
+    import aiohttp
+except ImportError
+    aiohttp = None
 import requests
 
 urls = ['http://' + x for x in 'apple.com ibm.com python.org pypy.org'.split()]
 url = urls[0]
 
 
-def async_test(func):
+def async_test(func):  # is func a coroutine function or a coroutine?
     '''https://docs.python.org/3/library/asyncio-task.html'''
     fmt = 'iscoroutine: {}\niscoroutinefunction: {}'
     return fmt.format(asyncio.iscoroutine(func),
@@ -13,13 +17,24 @@ def async_test(func):
 
 
 async def async_get_one(url):  # iscoroutinefunction
-    return requests.get(url).text[:100]  # iscoroutine
+    fmt = "{} async_get_one('{}')"
+    print(fmt.format(0, url))
+    coroutine = requests.get(url).text[:100]
+    print(fmt.format(1, url))
+    print(coroutine)
+    print(async_test(coroutine))
+    return coroutine  # iscoroutine
 
 
 async def async_get_many(urls):  # iscoroutinefunction
+    fmt = "{} async_get_many('{} urls')"
+    print(fmt.format(0, len(url)))
     pages = []
-    for coroutine in [async_get_one(url) for url in urls]:
+    for i, coroutine in enumerate([async_get_one(url) for url in urls]):
+        print('awaiting: {}'.format(i)
         pages.append(await coroutine)
+        print('awaited:  {}'.format(i)
+    print(fmt.format(1, len(url)))
     return pages  # iscoroutine
     # return ((await async_get_one(url)) for url in urls)  # iscoroutine
 
