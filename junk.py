f9b1ae5b5d7b2b8e865156c9d414b7f3d74f6a36
cclauss <cclauss@bluewin.ch>
2016-06-25 04:22:11 +0200

    junk.py

---
+++ junk.py
@@ -0,0 +1,23 @@
+class Channel(object):
+    def __init__(self, server, name, channel_id, members=None):
+        self.server = server
+        self.name = name
+        self.id = channel_id
+        self.members = members or []
+
+    def __eq__(self, compare_str):
+        if compare_str in (self.id, self.name) or "#" + compare_str == self.name:
+            return True
+        else:
+            return False
+
+    def __str__(self):
+        fmt = "{0} : {1:.40}"
+        return "\n".join(fmt.format(key, value) for key, value in self.__dict__.items())
+
+    def __repr__(self):
+        return self.__str__()
+
+    def send_message(self, message):
+        message_json = {"type": "message", "channel": self.id, "text": message}
+        self.server.send_to_websocket(message_json)
