21e3731cfde499272b9f4b310e26f824f566c531
cclauss <cclauss@bluewin.ch>
2016-07-04 09:18:54 +0200

    LOOP

--- fetch_one_fetch_many_requests.py
+++ fetch_one_fetch_many_requests.py
@@ -3,8 +3,7 @@
 import asyncio
 import requests
 
-loop = asyncio.get_event_loop()
-
+LOOP = asyncio.get_event_loop()
 URLS = '''http://python.org
           http://golang.org
           http://perl.org
@@ -13,7 +12,7 @@ URLS = '''http://python.org
 
 async def fetch_one(url):  # iscoroutinefunction
     print('fetch_one({})'.format(url))
-    return await loop.run_in_executor(None, requests.get, url)  # iscoroutine
+    return await LOOP.run_in_executor(None, requests.get, url)  # iscoroutine
 
 
 async def fetch_many(urls):  # iscoroutinefunction
@@ -21,6 +20,6 @@ async def fetch_many(urls):  # iscoroutinefunction
 
 
 print('=' * 20)
-pages = [resp.text for resp in loop.run_until_complete(fetch_many(URLS))]
+pages = [resp.text for resp in LOOP.run_until_complete(fetch_many(URLS))]
 for url, page in zip(URLS, pages):
     print('{}:\n\t{}\n'.format(url, page[:100].replace('\n', ' ')))
